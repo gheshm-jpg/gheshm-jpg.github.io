4d32897801e5734520f912f01a7871be113ed1bf
gheshm-jpg <gheshm@bc.edu>
2026-04-17 07:09:15 -0400

    Update firebase-config.js
    Firebase config object import

--- firebase-config.js
+++ firebase-config.js
@@ -3,17 +3,20 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
+
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyC6Z081EBLS4cEXwXrHp87JFWJnds0Mu0A",
+  authDomain: "gradepath-dc14f.firebaseapp.com",
+  projectId: "gradepath-dc14f",
+  storageBucket: "gradepath-dc14f.firebasestorage.app",
+  messagingSenderId: "267448889709",
+  appId: "1:267448889709:web:900001ebe023dbbc58aa64"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export services 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
